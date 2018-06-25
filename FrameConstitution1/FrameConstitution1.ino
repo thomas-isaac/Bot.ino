71deb8fecafc97e16c5a2d4a4b1c8afc44644134
Unknown <thomas.isaac@viacesi.fr>
2018-06-25 14:41:48 +0200

    Working Frame

--- FrameConstitution1/FrameConstitution1.ino
+++ FrameConstitution1/FrameConstitution1.ino
@@ -1,37 +1,45 @@
+#include <VirtualWire.h>
+#include <VirtualWire_Config.h>
+
 #define POLYNOMIAL 0xD8  /* 11011 followed by 0's */
 #define WIDTH  8
 #define TOPBIT 128
 
-String frame;
+uint8_t frame[6];
 
-unsigned int dst = 70;
-unsigned int src = 7;
+uint8_t dst = 70;
+uint8_t src = 7;
 
-uint8_t data[] = {42, 43,};
+uint8_t data[2] = {42, 43};
 uint8_t checksum;
+
+byte i;
+byte j;
 
 void setup() {
   Serial.begin(9600);
+  vw_set_tx_pin(8);
+  vw_setup(1500);
 }
 
 void loop() {
-  frame += dst;
-  frame += src;
+  frame[0] = dst;
+  frame[1] = src;
   
-  for(byte i =0; i <sizeof(data); i++){
-    data[i] = rot47(data[i]);
-    frame += data[i];
+  for(i=0; i <=sizeof(data); i++){
+    frame[i+2] = rot47(data[i]);
   }
 
   crcInit();
   checksum = crcFast(data, sizeof(data));
-  frame += checksum;
-
-  Serial.println(frame);
+  frame[i+1]= checksum;
+  
+  vw_send(frame, strlen((char*)frame));
+  vw_wait_tx();
 }
 
 char rot47(uint8_t var) {
-  var = (126 -var <=47) ? var -47 : var +47;
+  var = (var <=128) ? var +127 : var -127;
   return(var);
 }
 
